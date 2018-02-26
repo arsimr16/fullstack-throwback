00d9241e3d997ca008ed8e14245dde6e6b10b72b
arsimr16 <arsimr16@g.holycross.edu>
2018-02-25 22:18:51 -0800

    complete module 1, step 7

--- 1-servers/express-server.js
+++ 1-servers/express-server.js
@@ -6,3 +6,9 @@ Your server here! If you need help getting started,
 check out the express hello world documentation.
 https://expressjs.com/en/starter/hello-world.html
 */
+
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
